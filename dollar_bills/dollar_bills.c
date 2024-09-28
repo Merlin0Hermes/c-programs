769d37a9b4f9445b70610ffe90692f5357d8ac24
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-09-28 19:16:22 +0530

    add program

---
+++ dollar_bills/dollar_bills.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int amount, twenty, ten, five, one;
+
+    printf("Enter a dollar amount: ");
+    scanf("%d", &amount);
+
+    twenty = amount / 20;
+    amount -= (twenty * 20);
+
+    ten = amount / 10;
+    amount -= (ten * 10);
+
+    five = amount / 5;
+    amount -= (five * 5);
+
+    one = amount / 1;
+    
+    printf("$20 bills: %d\n", twenty);
+    printf("$10 bills: %d\n", ten);
+    printf(" $5 bills: %d\n", five);
+    printf(" $1 bills: %d\n", one);
+
+    return 0;
+
+}
