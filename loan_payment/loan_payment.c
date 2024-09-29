f6d091304a3c1bb26f5092033ced35b4c32b1dec
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-09-29 19:53:52 +0530

    restructure repetitive code into loop

--- loan_payment/loan_payment.c
+++ loan_payment/loan_payment.c
@@ -2,7 +2,9 @@
 
 int main(void)
 {
+    char *num_words[3] = {"first", "second", "third"};
     float amount, rate, payment;
+    int i;
 
     printf("Enter amount of loan: ");
     scanf("%f", &amount);
@@ -15,15 +17,11 @@ int main(void)
 
     rate = (rate / 100) / 12;
 
-    amount += rate * amount;
-    amount -= payment;
-    printf("Balance remaining after first payment: %.2f\n", amount);
+    for (i = 0; i < 3; i++)
+    {
+        amount += rate * amount;
+        amount -= payment;
+        printf("Balance remaining after %s payment: %.2f\n", num_words[i], amount);
+    }
 
-    amount += rate * amount;
-    amount -= payment;
-    printf("Balance remaining after second payment: %.2f\n", amount);
-
-    amount += rate * amount;
-    amount -= payment;
-    printf("Balance remaining after third payment: %.2f\n", amount);
 }
