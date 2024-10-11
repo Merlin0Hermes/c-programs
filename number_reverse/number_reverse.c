30b471519db5ede3e58dfe1fb7a2070abecb05ae
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-10-11 18:46:26 +0530

    add code for 3 digit numbers

--- number_reverse/number_reverse.c
+++ number_reverse/number_reverse.c
@@ -5,7 +5,10 @@ int main(void)
     int number, reverse = 0;
 
     printf("Enter a two digit number: ");
-    scanf("%2d", &number);
+    scanf("%3d", &number);
+
+    reverse = reverse * 10 + (number % 10);
+    number /= 10;
 
     reverse = reverse * 10 + (number % 10);
     number /= 10;
