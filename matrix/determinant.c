160b6d0c41a0ba6123129abdbd85af998824c65b
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-10-12 16:40:38 +0530

    modify function for square matrix

--- matrix/determinant.c
+++ matrix/determinant.c
@@ -1,23 +1,38 @@
 /*Finds determinant of a given matrix*/
 #include <stdio.h>
 
-void get_matrix(int matrix[10][10], int m, int n);
+void get_matrix(int matrix[10][10], int n);
+int determinant(int matrix[10][10], int n);
+
 
 int main(void)
 {
     int matrix[10][10];
+    int n;
 
-    get_matrix(matrix, 10, 10);
+    printf("Enter size of matrix n :");
+    scanf("%d", &n);
+
+    if (n > 10)
+    {
+        printf("Maximum size is 10: ");
+    }
+
+    get_matrix(matrix, 10);
 
 
 }
 
+int determinant(int matrix[10][10], int n)
+{
 
-void get_matrix(int matrix[10][10], int m, int n)
+}
+
+void get_matrix(int matrix[10][10], int n)
 {
     int i, j;
 
-    for (i = 0; i < m; i++)
+    for (i = 0; i < n; i++)
     {
         for (j = 0; j < n; j++)
         {
