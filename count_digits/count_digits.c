276ddf52fef61183f07a31b472d11f2eb2fe4ab0
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-10-13 13:50:31 +0530

    add code to check till four digits numbers

--- count_digits/count_digits.c
+++ count_digits/count_digits.c
@@ -2,5 +2,36 @@
 
 int main(void)
 {
-    
+    int number, original, digits;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    original = number;
+
+    if (original < 0)
+    {
+        number *= -1;
+
+    }
+
+    if (number < 10)
+    {
+        digits = 1;
+
+    }
+    else if (number < 100)
+    {
+        digits = 2;
+    }
+    else if (number < 1000)
+    {
+        digits = 3;
+    }
+    else if (number < 10000)
+    {
+        digits = 4;
+    }
+
+    printf("Your number %d has %d digits.\n", original, digits);
+    return 0;
 }
