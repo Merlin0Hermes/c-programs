d70f3a1de7852e24545e39b1cedafc7ed3858db9
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-10-19 17:53:26 +0530

    separate gcd calculation to its own function

--- gcd/gcd.c
+++ gcd/gcd.c
@@ -1,12 +1,20 @@
 /*Finds GCD of two integers*/
 #include <stdio.h>
 
+int gcd(int num1, int num2);
+
 int main(void)
 {
-    int num1, num2, remainder;
+    int num1, num2;
     printf("Enter two integers: ");
     scanf("%d %d", &num1, &num2);
 
+    printf("Greatest common divisor: %d\n", gcd(num1, num2));
+
+}
+int gcd(int num1, int num2)
+{
+    int remainder;
     while (1)
     {
         if (num2 == 0)
@@ -16,8 +24,6 @@ int main(void)
         remainder = num1 % num2;
         num1 = num2;
         num2 = remainder;
-
     }
-    printf("Greatest common divisor: %d\n", num1);
-
+    return num1;
 }
