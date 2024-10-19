9e9b638a73c26c06114a866c2a70dc24c7b6a821
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-10-19 17:34:22 +0530

    add basic template

---
+++ largest_num/largest_num.c
@@ -0,0 +1,24 @@
+/*Takes numbers from user and outputs largest number*/
+#include <stdio.h>
+
+int main(void)
+{
+    int number, max;
+
+
+    printf("Enter a number: ");
+    scanf("%d", &number); 
+    max = number; 
+
+    while (number > 0)
+    {
+        printf("Enter a number: ");
+        scanf("%d", &number);  
+
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+
+}
