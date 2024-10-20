a18d80b6f6ffe9171af6d0398832feba0ec26bda
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-10-20 13:46:02 +0530

    add function to find smallest element

--- matrix/min_element.c
+++ matrix/min_element.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #define SIZE 10
 
+void get_matrix(int matrix[SIZE][SIZE], int m, int n);
+void min_element(int matrix[SIZE][SIZE], int m, int n);
+
 int main(void)
 {
+    int row, col;
+    printf("Enter size of matrix (rows columns): ");
+    scanf("%d %d", &row, &col);
 
+
+}
+
+void min_element(int matrix[SIZE][SIZE], int m, int n)
+{
+    int i, j, min;
+    min = matrix[0][0];
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (matrix[i][j] < min)
+            {
+                min = matrix[i][j];
+            }
+        }
+    }
+    return min;
 }
 
 void get_matrix(int matrix[SIZE][SIZE], int m, int n)
